dd51f93adc4d35973f0319e750f1cdb8ebbb8033
tranhuuphu5 <161321995+tranvanphuoc8@users.noreply.github.com>
2025-02-24 20:23:25 +0700

    Add project files.

---
+++ EmployeePayrollDashboard/Controllers/NhanVienController.cs
@@ -0,0 +1,23 @@
+﻿using EmployeePayrollDashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeePayrollDashboard.Controllers
+{
+    namespace EmployeePayrollDashboard.Controllers
+    {
+        public class NhanVienController : Controller
+        {
+            private NhanVienDAL dal = new NhanVienDAL();
+            public ActionResult Index()
+            {
+                List<NhanVien> danhSach = dal.GetAllNhanVien();
+                return View(danhSach);
+            }
+        }
+    }
+}
